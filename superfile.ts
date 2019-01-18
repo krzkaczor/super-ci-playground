d6e4843e1ca18e9aed17a8f22dd126a8ee9ea3a0
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-18 01:09:04 +0100

    fix

--- superfile.ts
+++ superfile.ts
@@ -1,6 +1,6 @@
 const { buildSize } = require("build-size-super-plugin");
 
-module.exports = async function main() {
+module.exports.main = async function main() {
   await buildSize({
     path: "./build/static/js",
   });
