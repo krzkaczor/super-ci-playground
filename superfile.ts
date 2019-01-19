04ddf31aa39178039a96640e37855717fd3822ea
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-20 00:21:10 +0100

    add deployment link

--- superfile.ts
+++ superfile.ts
@@ -1,6 +1,6 @@
 const { join } = require("path");
 
-const { superCI } = require("super-ci");
+const { superCI, report } = require("super-ci");
 const { buildSize } = require("build-size-super-plugin");
 
 module.exports.main = async function main() {
@@ -8,5 +8,8 @@ module.exports.main = async function main() {
     path: "./build/static/js",
   });
 
-  await superCI.saveCollection("build", join(__dirname, "build"));
+  if (superCI.isPr()) {
+    await superCI.saveCollection("build", join(__dirname, "build"));
+    report("Branch deployment: ", `https://s3-eu-west-1.amazonaws.com/superci-bucket/91cf4ed2-b523-4cca-874c-0ee73f3b5a72/${superCI.context.currentSha}/build/index.html`);
+  }
 };
