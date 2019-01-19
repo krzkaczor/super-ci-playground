623d0eb9d9feedcba22b4569910b6c0ec33f3d5c
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-20 00:32:17 +0100

    bump super ci client version

--- superfile.ts
+++ superfile.ts
@@ -10,6 +10,6 @@ module.exports.main = async function main() {
 
   if (superCI.isPr()) {
     await superCI.saveCollection("build", join(__dirname, "build"));
-    report("Branch deployment: ", `https://s3-eu-west-1.amazonaws.com/superci-bucket/91cf4ed2-b523-4cca-874c-0ee73f3b5a72/${superCI.context.currentSha}/build/index.html`);
+    report(`Branch deployment: https://s3-eu-west-1.amazonaws.com/superci-bucket/91cf4ed2-b523-4cca-874c-0ee73f3b5a72/${superCI.context.currentSha}/build/index.html`);
   }
 };
