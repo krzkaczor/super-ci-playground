e193e76a0840a4079c946b0cccd6c6fcdb89a568
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-19 20:57:44 +0100

    upload website

--- superfile.ts
+++ superfile.ts
@@ -1,8 +1,10 @@
+import { superCI } from "super-ci";
 const { buildSize } = require("build-size-super-plugin");
 
 module.exports.main = async function main() {
   await buildSize({
     path: "./build/static/js",
   });
-};
 
+  await superCI.saveCollection("build", "./build");
+};
