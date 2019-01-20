2b3868fa1c39f17c41dcd0a17c46672d44c55f26
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-20 23:15:06 +0100

    storybook support

--- superfile.ts
+++ superfile.ts
@@ -1,5 +1,6 @@
 const { join } = require("path");
 
+const exec = require('await-exec')
 const { superCI, report } = require("super-ci");
 const { buildSize } = require("build-size-super-plugin");
 
@@ -8,8 +9,15 @@ module.exports.main = async function main() {
     path: "./build/static/js",
   });
 
+  
+
   if (superCI.isPr()) {
     await superCI.saveCollection("build", join(__dirname, "build"));
     report(`Branch deployment: https://s3-eu-west-1.amazonaws.com/superci-bucket/91cf4ed2-b523-4cca-874c-0ee73f3b5a72/${superCI.context.currentSha}/build/index.html`);
   }
 };
+
+function visReg() {
+  superCI.getCollection("storybook-vis-reg", join(__dirname, "base-screenshots"));
+  exec("yarn storybook:screenshots");
+}
