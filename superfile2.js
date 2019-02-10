bf8ad04122df86481ec34be388f7c3756615e7b4
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-02-10 12:57:21 +0100

    update dep

--- superfile2.js
+++ superfile2.js
@@ -1,5 +1,5 @@
 const { superCI } = require("super-ci");
 
 module.exports.main = async function main() {
-  superCI.report("LATER COMMENT THAT SHOULD BE MERGED!");
+  superCI.report("LATER COMMENT THAT SHOULD BE MERGED2!");
 }
